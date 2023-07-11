5eb826b0770b202149c0ab3c09035e340d0052ce
afolabi mabunmi <brownboy3047@gmail.com>
2023-07-11 16:40:46 +0100

    refactored some code

--- src/components/PublishPost.tsx
+++ src/components/PublishPost.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useCollection } from "../hooks/useCollection";
 import { db, auth } from "../firebase/config";
 import { doc, deleteDoc } from "firebase/firestore";
@@ -19,7 +18,6 @@ import { useAuthContext } from "../hooks/useAuthContext";
 const PublishPost = () => {
   const { documents: publishPost } = useCollection("post");
   const { user } = useAuthContext();
-  const [showContent, setShowContent] = useState(true);
 
   const deletePublishPost = async (id: string) => {
     const publishPostRef = doc(db, "post", id);
@@ -30,7 +28,7 @@ const PublishPost = () => {
   return (
     <div className="publish_post">
       <div className="publish_post_section_one">
-        <h2 onClick={() => setShowContent(!showContent)}>For you</h2>
+        <h2>For you</h2>
 
         <h2>Featured</h2>
 
@@ -38,49 +36,47 @@ const PublishPost = () => {
       </div>
 
       <main className="publish_post_section_two">
-        {showContent && (
-          <ul>
-            {publishPost &&
-              publishPost.map((post) => (
-                <li key={post.id}>
-                  <div className="publish_post_content">
-                    <div className="publish_post_img">
-                      <img src={avatar} alt="avatar" />
-                      {user && post.uid === auth.currentUser?.uid && (
-                        <p onClick={() => deletePublishPost(post.id)}>
-                          <BsTrash3 />
-                        </p>
-                      )}
-                    </div>
-
-                    <h3>{post.name}</h3>
-                    {post.image && (
-                      <img
-                        src={post.image}
-                        alt="post image"
-                        className="the_post_image"
-                      />
+        <ul>
+          {publishPost &&
+            publishPost.map((post) => (
+              <li key={post.id}>
+                <div className="publish_post_content">
+                  <div className="publish_post_img">
+                    <img src={avatar} alt="avatar" />
+                    {user && post.uid === auth.currentUser?.uid && (
+                      <p onClick={() => deletePublishPost(post.id)}>
+                        <BsTrash3 />
+                      </p>
                     )}
-                    <p>{post.postContent}</p>
                   </div>
 
-                  <div className="publish_post_comment">
-                    <div className="comment" title="comment">
-                      <FaRegComment />
-                    </div>
-                    <div className="like" title="like">
-                      <FcLike />
-                    </div>
-                    <div className="analytics" title="statistics">
-                      <SiSimpleanalytics />
-                    </div>
-                  </div>
+                  <h3>{post.name}</h3>
+                  {post.image && (
+                    <img
+                      src={post.image}
+                      alt="post image"
+                      className="the_post_image"
+                    />
+                  )}
+                  <p>{post.postContent}</p>
+                </div>
 
-                  <p className="publish_post_border"></p>
-                </li>
-              ))}
-          </ul>
-        )}
+                <div className="publish_post_comment">
+                  <div className="comment" title="comment">
+                    <FaRegComment />
+                  </div>
+                  <div className="like" title="like">
+                    <FcLike />
+                  </div>
+                  <div className="analytics" title="statistics">
+                    <SiSimpleanalytics />
+                  </div>
+                </div>
+
+                <p className="publish_post_border"></p>
+              </li>
+            ))}
+        </ul>
       </main>
     </div>
   );
--- src/pages/dashBoard/main/main_components/Feed.tsx
+++ src/pages/dashBoard/main/main_components/Feed.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 //firebase
 import { db, auth } from "../../../../firebase/config";
